aa118b7d698e081efa21e59cae57c181bc801dc4
Santiago Gómez <sgomezcusguen@gmail.com>
2017-04-07 14:15:26 -0500

    Replaces characters for spaces and splits them in an array

---
+++ PresidentialCandidates.py
@@ -0,0 +1,11 @@
+
+def normalize_speech(speech):
+    speech = speech.lower().replace(",", " ").replace(".", " ").replace("\n", " ").replace("  ", " ").replace("[", " ").replace("]", " ").replace(":", " ").split(" ");
+    print(speech);
+
+
+
+normalize_speech("""Tonight, as we mark the conclusion of our celebration of Black History Month, we are reminded of our Nation's path toward civil rights and the work that still remains. Recent threats targeting Jewish Community Centers and vandalism of Jewish cemeteries, as well as last week's shooting in Kansas City, remind us that while we may be a Nation divided on policies, we are a country that stands united in condemning hate and evil in all its forms.
+Each American generation passes the torch of truth, liberty and justice --- in an unbroken chain all the way down to the present.
+That torch is now in our hands. And we will use it to light up the world. I am here tonight to deliver a message of unity and strength, and it is a message deeply delivered from my heart.
+A new chapter of American Greatness is now beginning.""");
